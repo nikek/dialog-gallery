3544a195deb721fb0876f234f3fec0369e045fc6
Niklas Ek <ek.niklas@gmail.com>
2025-04-15 15:08:04 +0200

    styles and fixes

--- ne-dialog-gallery.js
+++ ne-dialog-gallery.js
@@ -3,12 +3,12 @@ const dialogTemplate = document.createElement("template");
 dialogTemplate.innerHTML = String.raw`
 <dialog>
   <section>
-    <img id="dialogImage" src="" alt="Image" />
-    <form id="dialogForm" action="#">
-      <button type="button" id="prev">&lt;</button>
-      <nav id="dialogNav"></nav>
-      <button type="button" id="next">&gt;</button>
-      <button type="button" id="closeBtn">Close</button>
+    <img class="ne-dialogImage" src="" alt="Image" />
+    <form class="ne-dialogForm" action="#">
+      <button type="button" class="ne-dialogPrev">←</button>
+      <nav class="ne-dialogNav"></nav>
+      <button type="button" class="ne-dialogNext">→</button>
+      <button type="button" class="ne-dialogClose">Close</button>
     </form>
   </section>
 </dialog>
@@ -16,13 +16,15 @@ dialogTemplate.innerHTML = String.raw`
 
 export class NeDialogGallery extends HTMLElement {
   #radios = [];
-
-  constructor() {
-    super();
-  }
+  #dialogForm;
+  #listeners = new ListenerStore(); // to remove on disconnect {el, type, listener}
 
   connectedCallback() {
     this.setup();
+  }
+
+  disconnectedCallback() {
+    this.#listeners.clear();
   }
 
   setup() {
@@ -32,80 +34,129 @@ export class NeDialogGallery extends HTMLElement {
     this.appendChild(dialog);
 
     const dialogForm = dialog.querySelector("form");
-    const dialogImage = dialog.querySelector("#dialogImage");
-    const dialogNav = dialog.querySelector("#dialogNav");
-    const dialogClose = dialog.querySelector("#closeBtn");
+    const dialogImage = dialog.querySelector(".ne-dialogImage");
+    const dialogNav = dialog.querySelector(".ne-dialogNav");
+    const dialogClose = dialog.querySelector(".ne-dialogClose");
+    this.#dialogForm = dialogForm;
 
     const thumbnails = this.querySelectorAll("a");
-    thumbnails.forEach((thumbnail, i) => {
+    thumbnails.forEach((thumb, i) => {
       const radio = document.createElement("input");
       radio.type = "radio";
       radio.name = "image";
       radio.id = `radio_${i}`;
-      radio.value = thumbnail.href;
+      radio.value = thumb.href;
       dialogNav.append(radio);
       this.#radios.push(radio);
 
-      thumbnail.addEventListener("click", (e) => {
+      const handleClick = (e) => {
         if (e.metaKey) {
           return;
         }
         e.preventDefault();
         dialog.showModal();
-        dialogImage.src = thumbnail.href;
+        dialogImage.src = thumb.href;
         radio.checked = true;
         radio.focus();
         dialogForm.dispatchEvent(new Event("change"));
-      }); // maybe open dialog during capture phase..
+      };
+
+      this.#listeners.add({ el: thumb, type: "click", cb: handleClick });
     });
 
-    // Reusable function to navigate using the arrow keys or prev/next buttons
-    const navigate = (direction) => {
-      const current = this.#radios.findIndex((radio) => radio.checked);
-      if (direction === "prev") {
-        const prevIndex = current === 0 ? this.#radios.length - 1 : current - 1;
-        this.#radios[prevIndex].checked = true;
-      } else if (direction === "next") {
-        const nextIndex = current === this.#radios.length - 1 ? 0 : current + 1;
-        this.#radios[nextIndex].checked = true;
-      }
-
-      dialogForm.dispatchEvent(new Event("change"));
-    };
-
     // Use keyboard navigation to navigate the images anywhere within the dialog
-    dialog.addEventListener("keydown", (e) => {
-      if (e.key === "ArrowLeft") {
-        navigate("prev");
-      } else if (e.key === "ArrowRight") {
-        navigate("next");
-      }
+    this.#listeners.add({
+      el: dialog,
+      type: "keydown",
+      cb: ({ key }) =>
+        (key === "ArrowLeft" && this.navigate("prev")) ||
+        (key === "ArrowRight" && this.navigate("next")),
     });
 
     // Use the prev and next buttons to navigate the images
-    const prevButton = dialog.querySelector("#prev");
-    prevButton.addEventListener("click", () => navigate("prev"));
-    const nextButton = dialog.querySelector("#next");
-    nextButton.addEventListener("click", () => navigate("next"));
+    const prevBtn = dialog.querySelector(".ne-dialogPrev");
+    const nextBtn = dialog.querySelector(".ne-dialogNext");
+    this.#listeners.add({
+      el: prevBtn,
+      type: "click",
+      cb: () => this.navigate("prev"),
+    });
+    this.#listeners.add({
+      el: nextBtn,
+      type: "click",
+      cb: () => this.navigate("next"),
+    });
 
     // Use the radio buttons to navigate the images
-    dialogForm.addEventListener("change", () => {
-      const radioValue = dialogForm.elements.namedItem("image").value;
-      if (radioValue) {
-        dialogImage.src = radioValue;
-      }
+    this.#listeners.add({
+      el: dialogForm,
+      type: "change",
+      cb: () => {
+        const radioValue = dialogForm.elements.namedItem("image").value;
+        if (radioValue) {
+          dialogImage.src = radioValue;
+        }
+      },
     });
 
     // Light dismiss the dialog when clicking on the backdrop
-    dialog.addEventListener("click", (e) => {
-      if (e.target.nodeName === "DIALOG") dialog.close("dismiss");
-    });
-
-    dialog.addEventListener("close", () => {
-      dialogImage.src = "";
+    this.#listeners.add({
+      el: dialog,
+      type: "click",
+      cb: ({ target: { nodeName } }) =>
+        nodeName === "DIALOG" && dialog.close("dismiss"),
     });
 
     // Close with the close button
-    dialogClose.addEventListener("click", () => dialog.close());
+    this.#listeners.add({
+      el: dialogClose,
+      type: "click",
+      cb: () => dialog.close(),
+    });
+
+    // Close the dialog when the dialog is closed
+    this.#listeners.add({
+      el: dialog,
+      type: "close",
+      cb: () => (dialogImage.src = ""),
+    });
+  }
+
+  // Reusable method to navigate using the arrow keys or prev/next buttons
+  navigate(direction) {
+    const current = this.#radios.findIndex((radio) => radio.checked);
+    if (direction === "prev") {
+      const prevIndex = current === 0 ? this.#radios.length - 1 : current - 1;
+      this.#radios[prevIndex].checked = true;
+    } else if (direction === "next") {
+      const nextIndex = current === this.#radios.length - 1 ? 0 : current + 1;
+      this.#radios[nextIndex].checked = true;
+    }
+
+    this.#dialogForm.dispatchEvent(new Event("change"));
+  }
+}
+
+// A simple store to manage event listeners, created to easily remove listeners on disconnectedCallback
+class ListenerStore {
+  #listeners = [];
+
+  add(listener) {
+    const { el, type, cb } = listener;
+    el.addEventListener(type, cb);
+    this.#listeners.push(listener);
+  }
+
+  remove(listener) {
+    const { el, type, cb } = listener;
+    el.removeEventListener(type, cb);
+    this.#listeners = this.#listeners.filter((l) => l !== listener);
+  }
+
+  clear() {
+    this.#listeners.forEach(({ el, type, cb }) => {
+      el.removeEventListener(type, cb);
+    });
+    this.#listeners = [];
   }
 }
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Dialog Gallery
+
+Proof of concept web component that wraps linked images and progressively enhance the list into a carousel interface in a top-level dialog, using radio buttons to select image.
